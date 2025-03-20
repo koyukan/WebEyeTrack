d3258b31f54566dd14b79fa88e0d9c57a34cf9ee
edavalosanaya <davalosaeduardo@gmail.com>
2025-03-20 16:42:35 -0500

    Finished gaze capture data loading.

--- python/scripts/preprocessing/gazecapture.py
+++ python/scripts/preprocessing/gazecapture.py
@@ -2,13 +2,14 @@
 References
 https://github.com/swook/faze_preprocess/blob/master/create_hdf_files_for_faze.py
 """
-
+import os
 import pathlib
 from typing import List, Dict, Union, Optional, Tuple
 import shutil
 import json
 from dataclasses import asdict
 import pickle
+from collections import defaultdict
 
 import pandas as pd
 from tqdm import tqdm
@@ -16,11 +17,18 @@ import cv2
 from PIL import Image
 import yaml
 import numpy as np
-from torch.utils.data import Dataset
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+from webeyetrack.model_based import (
+    estimate_face_width, 
+    face_reconstruction,
+    create_perspective_matrix,
+    estimate_camera_intrinsics,
+    estimate_gaze_origins,
+    create_transformation_matrix
+)
 from webeyetrack.constants import GIT_ROOT
 from webeyetrack.data_protocols import Annotations, CalibrationData, Sample
 
@@ -28,7 +36,7 @@ CWD = pathlib.Path(__file__).parent
 LEFT_EYE_LANDMARKS = [263, 362, 386, 374, 380]
 RIGHT_EYE_LANDMARKS = [33, 133, 159, 145, 153]
 
-class GazeCaptureDataset(Dataset):
+class GazeCaptureDataset():
 
     def __init__(
         self,
@@ -72,8 +80,10 @@ class GazeCaptureDataset(Dataset):
         participant_dir = [x for x in self.dataset_dir.iterdir() if x.is_dir()]
 
         # Saving information
-        self.samples: List[Sample] = []
-        self.participant_calibration_data: Dict[CalibrationData] = {}
+        # self.samples: List[Sample] = []
+        self.samples = defaultdict(list)
+        self.sample_calibration_data: List[CalibrationData] = []
+        # self.participant_calibration_data: Dict[CalibrationData] = {}
 
         # Open the ``frames.json`` file with a list of frame names
         num_samples = 0
@@ -89,8 +99,17 @@ class GazeCaptureDataset(Dataset):
             with open(part_dir / 'dotInfo.json', 'r') as f:
                 dot_info = json.load(f)
             dot_info_df = pd.DataFrame(dot_info)
+            with open(part_dir / 'screen.json', 'r') as f:
+                screen_info = json.load(f)
+            screen_info_df = pd.DataFrame(screen_info)
             
             per_participant_samples = 0
+            face_width_cm = None
+            intrinsics = None
+            height, width = None, None
+
+            # Create a directory for the generated samples
+            os.makedirs(part_dir / 'samples', exist_ok=True)
 
             for i, frame in tqdm(enumerate(frames_fname_list), total=len(frames_fname_list)):
 
@@ -102,6 +121,80 @@ class GazeCaptureDataset(Dataset):
 
                 frame_fp = part_dir / 'frames' / frame
                 dot_info = dot_info_df.iloc[i]
+                """
+                DotNum      0.000000
+                XPts      280.000000
+                YPts      528.000000
+                XCam        1.938750
+                YCam       -9.467451
+                Time        0.237868
+                Name: 0, dtype: float64
+                """
+                screen_info = screen_info_df.iloc[i]
+                """
+                H              568
+                W              320
+                Orientation      1
+                Name: 0, dtype: int64
+                """
+
+                # Compute the location of the camera with respect to the screen
+                # This needs to consider the orientation of the screen
+                # Assume the camera is at the top center of the screen (in portrait mode)
+                # Compute the PoG
+                pog_px = np.array([dot_info['XPts'], dot_info['YPts']])
+                pog_cm = np.array([dot_info['XCam'], dot_info['YCam']])
+                s_width, s_height = screen_info['W'], screen_info['H']
+                pog_norm = np.array([pog_px[0] / s_width, pog_px[1] / s_height])
+                s_width_cm, s_height_cm = pog_cm[0] / s_width, pog_cm[1] / s_height
+
+                """
+                Orientation: The orientation of the interface, as described by the enumeration UIInterfaceOrientation, where:
+                1: portrait
+                2: portrait, upside down (iPad only)
+                3: landscape, with home button on the right
+                4: landscape, with home button on the left
+                """
+                if screen_info['Orientation'] == 1:
+                    camera_location = np.array([s_width_cm/2, 0, 0])
+                elif screen_info['Orientation'] == 2:
+                    camera_location = np.array([s_width_cm/2, s_height_cm, 0])
+                elif screen_info['Orientation'] == 3:
+                    camera_location = np.array([0, s_width_cm/2, 0])
+                elif screen_info['Orientation'] == 4:
+                    camera_location = np.array([s_height_cm, s_width_cm/2, 0])
+                else:
+                    raise ValueError(f"Orientation {screen_info['Orientation']} is not recognized.")
+
+                screen_RT = create_transformation_matrix(
+                    scale=np.array([1, 1, 1]),
+                    translation=camera_location,
+                    rotation=np.array([0, 0, 0])
+                )
+
+                # Create calibration data
+                calibration_data = CalibrationData(
+                    camera_retval=0,
+                    camera_matrix=intrinsics,
+                    dist_coeffs=np.zeros((5, 1)),
+                    screen_RT=screen_RT,
+                    monitor_height_cm=s_height_cm,
+                    monitor_height_px=s_height,
+                    monitor_width_cm=s_width_cm,
+                    monitor_width_px=s_width
+                )
+
+                # If the annotation already exists, create the sample that is referenced
+                annotation_fp = part_dir / 'samples' / f'{frame}.pkl'
+                if annotation_fp.exists():
+                    self.samples['id'].append(f"{participant_id}_{i}")
+                    self.samples['participant_id'].append(participant_id)
+                    self.samples['image_fp'].append(frame_fp)
+                    self.samples['annotation_fp'].append(annotation_fp)
+                    self.sample_calibration_data.append(calibration_data)
+                    num_samples += 1
+                    per_participant_samples += 1
+                    continue
 
                 # Load the image
                 image_np = cv2.imread(str(frame_fp))
@@ -128,100 +221,103 @@ class GazeCaptureDataset(Dataset):
                 # cv2.imshow('image', image_landmarks)
                 # cv2.waitKey(0)
 
-                # Compute the gaze origin (between the eyes) in 2D
-                data_2d_pairs = {
-                    'left': face_landmarks[LEFT_EYE_LANDMARKS] * np.array([image_np.shape[0], image_np.shape[1]]),
-                    'right': face_landmarks[RIGHT_EYE_LANDMARKS] * np.array([image_np.shape[0], image_np.shape[1]])
-                }
+                # Estimate the face width
+                if face_width_cm is None:
+                    face_width_cm = estimate_face_width(face_landmarks_all[:, :2], face_landmarks_rt)
+                if intrinsics is None:
+                    # Use the first frame to estimate the intrinsics
+                    height, width, _ = image_np.shape
+                    intrinsics = estimate_camera_intrinsics(np.zeros((height, width, 3)))
 
-                # Compute the eye origin in 2D
-                eye_origins_2d = {}
-                for k, v in data_2d_pairs.items():
-                    eye_origins_2d[k] = np.mean(v, axis=0)
+                facial_landmarks_px = face_landmarks_all[:, :2] * np.array([width, height])
 
-                # Compute the face origin in 2D
-                face_origin_2d = np.mean([eye_origins_2d['left'], eye_origins_2d['right']], axis=0)
+                perspective_matrix = create_perspective_matrix(aspect_ratio=image_np.shape[1] / image_np.shape[0])
+                inv_perspective_matrix = np.linalg.inv(perspective_matrix)
 
-                # Compute the bounding box
-                face_bbox = np.array([
-                    int(np.min(face_landmarks[:, 1])), 
-                    int(np.min(face_landmarks[:, 0])), 
-                    int(np.max(face_landmarks[:, 1])), 
-                    int(np.max(face_landmarks[:, 0]))
-                ])
+                # Perform 3D face reconstruction and determine the pose in 3D centimeters
+                metric_transform, metric_face = face_reconstruction(
+                    perspective_matrix=perspective_matrix,
+                    face_landmarks=face_landmarks_all[:, :3],
+                    face_width_cm=face_width_cm,
+                    face_rt=face_landmarks_rt,
+                    K=intrinsics,
+                    frame_height=height,
+                    frame_width=width,
+                    frame=image_np
+                )
 
-                # Apply the facial translation to the landmarks
-                t_face_landmarks_xyz = face_landmarks_all[:, :3].copy()
-                t_face_landmarks_xyz = t_face_landmarks_xyz + face_landmarks_rt[:3, 3]
+                # Obtain the gaze origins based on the metric face pts
+                gaze_origins = estimate_gaze_origins(
+                    face_landmarks_3d=metric_face,
+                    face_landmarks=facial_landmarks_px,
+                )
 
-                # Compute the gaze origin (between the eyes)
-                data_3d_pairs = {
-                    'left': t_face_landmarks_xyz[LEFT_EYE_LANDMARKS][:, :3],
-                    'right': t_face_landmarks_xyz[RIGHT_EYE_LANDMARKS][:, :3]
-                }
+                # Construct a 3D gaze target based on the dot information
+                gaze_target_3d = np.array([dot_info['XCam'], dot_info['YCam'], 0])
 
-                # Create the 3D gaze target from the dot
-                gaze_3d_target = np.array([dot_info['XCam'], dot_info['YCam'], 0])
-                # import pdb; pdb.set_trace()
-
-                # Compute the 3D eye origins and vectors
-                origins_3d = {}
-                eye_gaze_vectors = {}
-                for k, v in data_3d_pairs.items():
-                    origins_3d[k] = np.mean(v, axis=0)
-
-                    # Compute the gaze vector from the eye origin to the gaze target
-                    gaze_vector = gaze_3d_target - origins_3d[k]
-                    gaze_vector = gaze_vector / np.linalg.norm(gaze_vector)
-                    eye_gaze_vectors[k] = gaze_vector
-
-                # Compute the face gaze vector
-                face_gaze_vector = np.mean([eye_gaze_vectors['left'], eye_gaze_vectors['right']], axis=0)
-                face_gaze_vector = face_gaze_vector / np.linalg.norm(face_gaze_vector)
-
-                # Compute the face origin
-                face_origin_3d = np.mean([origins_3d['left'], origins_3d['right']], axis=0)
+                # Compute the gaze vector for each eye between origin and target
+                face_gaze_vector = gaze_target_3d - gaze_origins['face_origin_3d']
 
                 # Convert face_blendshapes to a proper numpy array
                 np_face_blendshapes = np.array([x.score for x in face_blendshapes])
 
-                # Create the annotation
                 annotation = Annotations(
                     original_img_size=np.array(image_np.shape),
+                    intrinsics=intrinsics,
+                    # Facial landmarks
+                    facial_detection_results=detection_results,
                     facial_landmarks=face_landmarks_all,
                     facial_landmarks_2d=face_landmarks,
                     facial_rt=face_landmarks_rt,
                     face_blendshapes=np_face_blendshapes,
-                    face_bbox=face_bbox,
-                    head_pose_3d=np.array([0, 0, 0, 0, 0, 0]), # From MPIIFaceGaze, not GazeCapture
-                    face_origin_3d=face_origin_3d,
-                    face_origin_2d=face_origin_2d,
-                    gaze_direction_3d=face_gaze_vector,
-                    gaze_target_3d=gaze_3d_target,
-                    gaze_target_2d=np.array([dot_info['XCam'], dot_info['YCam']]),
-                    pog_px=np.array([dot_info['XPts'], dot_info['YPts']])
+                    face_bbox=np.array([]), # Not important
+                    head_pose_3d=np.array([]),
+                    # Face Gaze
+                    face_origin_3d=gaze_origins['face_origin_3d'],
+                    face_origin_2d=gaze_origins['face_origin_2d'].flatten(),
+                    face_gaze_vector=face_gaze_vector,
+                    # Eye Gaze
+                    left_eye_origin_3d=np.empty((3, 0), dtype=np.float32),
+                    left_eye_origin_2d=np.empty((2, 0), dtype=np.float32),
+                    left_gaze_vector=np.empty((3, 0), dtype=np.float32),
+                    right_eye_origin_3d=np.empty((3, 0), dtype=np.float32),
+                    right_eye_origin_2d=np.empty((2, 0), dtype=np.float32),
+                    right_gaze_vector=np.empty((3, 0), dtype=np.float32),
+                    # Target information
+                    gaze_target_3d=gaze_target_3d,
+                    gaze_target_2d=np.zeros((2, 0), dtype=np.float32),
+                    pog_px=pog_px,
+                    pog_norm=pog_norm,
+                    pog_cm=pog_cm,
+                    # Gaze State Information
+                    is_closed=np.array([False])
                 )
 
-                # Create a sample
-                sample = Sample(
-                    participant_id=participant_id,
-                    image_fp=frame_fp,
-                    annotations=annotation,
-                )
+                # Save the annotation
+                with open(annotation_fp, 'wb') as f:
+                    pickle.dump(annotation, f)
 
-                self.samples.append(sample)
+                self.samples['id'].append(f"{participant_id}_{i}")
+                self.samples['participant_id'].append(participant_id)
+                self.samples['image_fp'].append(frame_fp)
+                self.samples['annotation_fp'].append(annotation_fp)
+                self.sample_calibration_data.append(calibration_data)
                 num_samples += 1
                 per_participant_samples += 1
+        
+        # Convert the samples to a DataFrame
+        self.samples = pd.DataFrame(self.samples)
 
     def __getitem__(self, index: int):
         sample = self.samples.iloc[index]
+        calibration_data = self.sample_calibration_data[index]
 
         # Load image
         image = Image.open(sample.image_fp)
         image_np = np.array(image)
 
         # Get the calibration
-        calibration_data = self.participant_calibration_data[sample.participant_id]
+        # calibration_data = self.participant_calibration_data[sample.participant_id]
 
         # Load the annotations
         with open(sample.annotation_fp, 'rb') as f:
@@ -244,18 +340,19 @@ class GazeCaptureDataset(Dataset):
     def __len__(self):
         return len(self.samples)
     
-# if __name__ == "__main__":
+if __name__ == "__main__":
     
-#     from ..constants import DEFAULT_CONFIG
-#     with open(DEFAULT_CONFIG, 'r') as f:
-#         config = yaml.safe_load(f)
+    from webeyetrack.constants import DEFAULT_CONFIG
+    with open(DEFAULT_CONFIG, 'r') as f:
+        config = yaml.safe_load(f)
 
-#     dataset = GazeCaptureDataset(
-#         dataset_dir=GIT_ROOT / pathlib.Path(config['datasets']['GazeCapture']['path']),
-#         per_participant_size=10,
-#         dataset_size=20
-#     )
-#     print(len(dataset))
+    dataset = GazeCaptureDataset(
+        dataset_dir=GIT_ROOT / pathlib.Path(config['datasets']['GazeCapture']['path']),
+        per_participant_size=10,
+        dataset_size=20
+    )
+    print(len(dataset))
 
-#     sample = dataset[0]
-#     print(json.dumps({k: str(v.dtype) for k, v in sample.items()}, indent=4))
+    sample = dataset[0]
+    # print(json.dumps({k: str(v.dtype) for k, v in sample.items()}, indent=4))
+    print(dataset.samples.head())
