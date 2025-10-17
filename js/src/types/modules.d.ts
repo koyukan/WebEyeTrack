59acaafbad3b97638b140c58518bc3d69e4456f3
Huseyin Koyukan <kaan@imerit.net>
2025-10-17 22:05:47 +0300

    Create centralized type declaration infrastructure
    Set up proper type declaration system with dedicated types/ directory.
    Add comprehensive worker-loader module declarations supporting webpack
    query parameters. Remove deprecated typings/ directory.

---
+++ js/src/types/modules.d.ts
@@ -0,0 +1,27 @@
+/**
+ * Type declarations for webpack module loaders and external modules.
+ * This file provides TypeScript support for non-standard module imports.
+ */
+
+/**
+ * Declaration for worker-loader with query parameters.
+ * Supports webpack worker-loader syntax like: worker-loader?inline=no-fallback!./Worker.ts
+ *
+ * @see https://github.com/webpack-contrib/worker-loader
+ */
+declare module 'worker-loader?*' {
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+  export default WebpackWorker;
+}
+
+/**
+ * Fallback declaration for standard worker-loader imports.
+ */
+declare module 'worker-loader!*' {
+  class WebpackWorker extends Worker {
+    constructor();
+  }
+  export default WebpackWorker;
+}
